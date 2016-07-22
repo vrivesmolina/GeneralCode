eec5392524def6d23fdc522471466cbde4dbc76d
Vicente Rives Molina <vicente.rives.molina@gmail.com>
2016-07-22 02:37:31 +0200

    Creation of binomialEfficiency.py

---
+++ binominalEfficiency.py
@@ -0,0 +1,26 @@
+import math
+from uncertainties import ufloat
+
+def askForValues():
+    input_numerator = raw_input('what is your numerator?: ')
+    input_denominator = raw_input('what is your denominator?: ')
+    if float(input_numerator) > float(input_denominator):
+        print 'The numerator can\'t be higher than the denominator! Try again! '
+        del input_numerator, input_denominator
+        input_numerator, input_denominator = askForValues()
+    return input_numerator, input_denominator
+
+def calculateEfficiency(num, den):
+    if num < den:
+        eff = num/den
+        err = 1/den * math.sqrt(num * (1-eff))
+    elif num == den:
+        eff = 1.0
+        err = 1.0
+    return eff, err
+
+x, y = askForValues()
+efficiency, error = calculateEfficiency(float(x), float(y))
+tot_eff = ufloat(efficiency, error)
+
+print 'The efficiency is', tot_eff
